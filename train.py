2813cfbe7998f2555c6f55aacb25d102d3bdb9b5
vdoorn <118536179+vdoorn@users.noreply.github.com>
2024-04-30 09:52:43 +0200

    Add files via upload

--- train.py
+++ train.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from Main import cnnnet
-from Dataloader import DataReader
+from torch.utils.data import Dataset
+from Main import cnnnet1
+from torch.utils.data import DataLoader
 import numpy as np
 import matplotlib.pyplot as plt 
 import os
@@ -12,10 +13,11 @@ torch.manual_seed(seed)
 
 #-----HYPERPARAMETERS/training-----#
 block_size = 512
-batch_size = 128
+batch_size = 256
+test_size = 12
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 learning_rate = 1e-5
-max_iters = 6000
+max_iters = 500
 eval_interval = 100
 #-----HYPERPARAMETERS/training-----#
 
@@ -24,7 +26,7 @@ eval_interval = 100
 #-----DataLoader-----#
 arr = os.listdir("C:\\Users\\vd00r\\OneDrive\\Desktop\\Bap\\Data")
 
-for _ in arr:
+'''for _ in arr:
       arg = "C:\\Users\\vd00r\\OneDrive\\Desktop\\Bap\\Data\\" + _
       file1 = DataReader(dataset = arg)
       data = file1.load_data()
@@ -44,15 +46,24 @@ for _ in arr:
           targets = torch.cat((targets,targets_batch),dim = 0)
 logits = logits[:,None,:,:]
 torch.save(logits,"logits.pt")
-torch.save(targets,"targets.pt")
+torch.save(targets,"targets.pt")'''
 
 #-----DataLoader-----#
+logits = torch.load("logits.pt")
+targets = torch.load("targets.pt")
+dataset = torch.utils.data.TensorDataset(logits,targets)
+t,f,h,l = logits.shape
+train_size = int(t*0.9)
+train, test = torch.utils.data.random_split(dataset,[train_size,t-train_size])
+train = DataLoader(train,batch_size = batch_size,shuffle = True)
+test = DataLoader(test,batch_size = test_size,shuffle = True)
 
-print(logits.shape)
-print(targets.shape)
+f_list,t_list = next(iter(train))
+tf_list,tt_list = next(iter(test))
 #-----Model-----#
-model = cnnnet().to(device)
-#model.load_state_dict(torch.load("C:\\Users\\Gebruiker\\Desktop\\Bap\\Code\\cnnnet.pt"))
+arr = os.listdir("C:\\Users\\vd00r\\OneDrive\\Desktop\\Bap\\code")
+model = cnnnet1().to(device)
+#model.load_state_dict(torch.load("cnnnet1.pt"))
 model.train()
 #-----Model-----#
 
@@ -68,28 +79,30 @@ loss = torch.nn.CrossEntropyLoss()
 #print(lossvalue)
 
 llist = []
+tlist = []
 for iter in range(max_iters):
         # evaluate the loss
-        t,f,h,l = logits.shape
-        ind = torch.randperm(t)[:batch_size]
-        batch_list = logits[ind]
-        targets_list = targets[ind]
         #print(batch_list.shape)
         #print(targets_list.shape)
-        inputs = model(batch_list.to(device))
+        inputs = model(f_list.to(device))
+        inputst = model(tf_list.to(device))
         print(inputs[0])
-        lossvalue = F.cross_entropy(inputs, targets_list.to(device))
-        print(lossvalue)
+        print(t_list[0])
+        lossvalue = F.cross_entropy(inputs, t_list.to(device))
+        losstest = F.cross_entropy(inputst, tt_list.to(device))
+        print(lossvalue,losstest)
         llist.append(lossvalue.data.cpu().numpy())
+        tlist.append(losstest.data.cpu().numpy())
         optimizer.zero_grad(set_to_none=True)
         lossvalue.backward()
         optimizer.step()
         
-torch.save(model.state_dict(),'cnnnet.pt')
+torch.save(model.state_dict(),'cnnnet2048.pt')
 
 plt.figure(figsize=(10,5))
 plt.title("Training Loss")
 plt.plot(llist,label="train")
+plt.plot(tlist,label="val")
 plt.xlabel("iterations")
 plt.ylabel("Loss")
 plt.legend()
