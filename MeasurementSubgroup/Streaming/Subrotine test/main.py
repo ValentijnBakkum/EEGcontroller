651743c0c858682fa73115db9bf5b552d19f1000
C0baltius <martinlittle11@gmail.com>
2024-05-14 16:45:49 +0200

    Created some files to prototype the subprocess implementation and it works.

---
+++ MeasurementSubgroup/Streaming/Subrotine test/main.py
@@ -0,0 +1,11 @@
+import subprocess
+
+process = subprocess.Popen(["python3", "-u", "MeasurementSubgroup/Streaming/Subrotine test/sub.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE,)
+
+
+
+character = process.stdout.read1(1)
+process.stdin.write(b"G\n") # G for go
+#process.stdin.flush()
+
+process.communicate()
