569cd1da8b1df1543bdaaf71c825d57716eea00e
jackchen27 <jack.chen2712@gmail.com>
2024-05-22 20:35:05 +0200

    Add label column (not tested yet)

--- MeasurementSubgroup/Streaming/LSL_csv.py
+++ MeasurementSubgroup/Streaming/LSL_csv.py
@@ -14,6 +14,11 @@ inlet = StreamInlet(streams[0])
 columns = ['FZ', 'C3', 'CZ', 'C4', 'PZ', 'PO7', 'OZ', 'PO8', 'Counter','Validation']
 columns_index_dict = {'FZ': 0, 'C3': 1, 'CZ': 2, 'C4': 3,'PZ': 4, 'PO7': 5, 'OZ': 6, 'PO8': 7, 'Counter': 15, 'Validation': 16}
 
+pageArray = [1,2,3,4 ,4,3,2,1 ,2,3,4,1 ,1,3,4,2 ,3,2,4,1 ,4,1,2,3, 0]
+# 1. Right hand, 2. Left hand, 3. Tongue, 4. Feet, 0. Rest
+label = []
+i = 0
+
 while True:
    data_dict = dict((k, []) for k in columns)
 
@@ -32,6 +37,20 @@ while True:
       # updating data dictionary with newly transmitted samples   
       for column in data_dict.keys(): # iterate over the columns of the data dictionary
          data_dict[column].append(all_data[columns_index_dict[column]]) # append the data to the corresponding column
+
+      ### Add label column
+      if data_dict['Counter'] % (250*(6+6)) == 0: # Each prompt takes 12 seconds in total. Set pageNumber to 0 which is the rest state
+         pageNumber = 0
+      elif data_dict['Counter'] % (250*(6)) == 0: # After 6 seconds the action will appear. Set pageNumber to pageArray[i] which is the action
+         pageNumber = pageArray[i]
+      else: # Else assign the same pageNumber
+         pageNumber = pageNumber
+
+      label.append(pageNumber) # Append to list label
+
+      # After 12 seconds add 1 to i, which goes to the next prompt
+      if data_dict['Counter'] % (250*(6+6)) == 0 and data_dict['Counter'] != 0 :
+         i += 1
       
       # data is collected at 250 Hz. Let's stop data collection after 60 seconds. Meaning we stop when we collected 250*60 samples.
       if len(data_dict['Counter']) >= 72005: # 72005 = sampling_rate * seconds_per_prompt * num_prompts + 5_seconds_leeway
@@ -40,6 +59,9 @@ while True:
    # lastly, we can save our data to a CSV format.
    data_df = pd.DataFrame.from_dict(data_dict) 
    now = datetime.now()
+   
+   # Add column to the data_df called label
+   data_df["Label"] = label # add new column to the Dataframe
 
    data_df.to_csv('MeasurementSubgroup/Our_measurements/EEGdata-' + now.strftime("%Y-%j--%H-%M-%S") + '.csv', index = False)
    finished = False
