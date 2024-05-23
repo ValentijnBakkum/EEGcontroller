43675ca14c3028e6490d2487fa65818a19d80704
vdoorn <118536179+vdoorn@users.noreply.github.com>
2024-05-23 23:55:59 +0200

    Updated B-spline/ status: in progress

--- MLsubgroup/bspline.py
+++ MLsubgroup/bspline.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 
+#Created/modified B-spline activation function from modification of files in github Repo of Kolmogorov-Arnold Networks
+#Link: https://github.com/KindXiaoming/pykan/blob/master/kan/spline.py
+
 class Bspline_segment_calc(nn.Module):
     def __init__(self,grid = 10,k = 0,extend = True,device = "cpu"):
         super().__init__()
@@ -20,30 +23,22 @@ class Bspline_segment_calc(nn.Module):
                 grid = torch.cat([grid[:,[0]] - h, grid],dim=1)
                 grid = torch.cat([grid, grid[:,[-1]] + h],dim=1)
             grid = grid.to(device)
-            print(grid.shape)
             return grid
         else:
             return grid
     
     #-----Calculate Bi in B-spline formula-----#    
     def Cox_deBoor(self,x,grid,k,extend,device):
-        print(k)
-        
-        extend = extend
         if extend == True:
             grids = self.make_grid() # make grid
             grids = self.extend_grid(grids) # extend grid
             grids = grids.unsqueeze(dim=2).to(device)
-            print(grids.shape)
         else:
-            print(grid.shape)
             grids = grid.unsqueeze(dim=2).to(device)
-            print(grids.shape)
         x = x.unsqueeze(dim=1).to(device)
         if k == 0:
             value = (x >= grids[:,:-1]) * (x < grids[:,1:])
         else:
-            print(grids.shape)
             B_km1 = self.Cox_deBoor(x = x[:,0],grid = grids[:,:,0],k = k - 1,extend = False, device = device)
             value = (x - grids[:, :-(k + 1)]) / (grids[:, k:-1] - grids[:, :-(k + 1)]) * B_km1[:, :-1] + (
             grids[:, k + 1:] - x) / (grids[:, k + 1:] - grids[:, 1:(-k)]) * B_km1[:, 1:]
@@ -53,4 +48,4 @@ bspline = Bspline_segment_calc(grid = 10,k = 3, extend = True)
 #grid = bspline.make_grid()
 #grid = bspline.extend_grid(True,3,grid)
 #print(grid)
-print(bspline.Cox_deBoor(torch.rand((5,100)),10,3,True,"cpu"))
+print(bspline.Cox_deBoor(torch.rand((5,20)),10,3,True,"cpu").shape)
