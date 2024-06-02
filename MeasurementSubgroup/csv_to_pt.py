ae31e62d6e7dba6f81b4e8d502be8a7f0667ef31
jackchen27 <jack.chen2712@gmail.com>
2024-06-02 23:38:08 +0200

    Update csv_to_pt.py

--- MeasurementSubgroup/csv_to_pt.py
+++ MeasurementSubgroup/csv_to_pt.py
@@ -49,33 +49,48 @@ def filter(df_input):
     return df_filt1
 
 def CAR_filter(logits):
-    a, b = logits.shape
-    
-    # initialise zero array to hold channel average
-    channel_average = np.zeros((a, b))
+    a,b,c = logits.shape
+    # initialise zero tensor to hold channel average
+    channel_average = torch.zeros(2880, 256)
 
     # compute average for each channel
-    for i in range(a):
-        for j in range(b):
-            channel_average[i, j] = np.mean(logits[i, j, :])
+    for i in range(int(logits.size(0))):
+        for j in range(int(logits.size(1))):
+            for k in range(int(logits.size(2))):
+                channel_average[i][j] += logits[i][j][k]
+            channel_average[i][j] = channel_average[i][j] / 16
     
-    # initialise empty array to hold filtered logits
-    logits_CAR = np.empty_like(logits)
+    # initalise empty tensor to hold filtered logits
+    logits_CAR = torch.empty(int(logits.size(0)), int(logits.size(1)), int(logits.size(2)))
 
     # subtract all readings by channel average to create new 'reference' signals
-    for i in range(a):
-        for j in range(b):
-            logits_CAR[i, j, :] = logits[i, j, :] - channel_average[i, j]
+    for i in range(int(logits.size(0))):
+        for j in range(int(logits.size(1))):
+            for k in range(int(logits.size(2))):
+                logits_CAR[i][j][k] = logits[i][j][k] - channel_average[i][j]
     
     return logits_CAR
 
 # Read the CSV file into a NumPy array
 data_csv = pd.read_csv('C:/Users/JackC/Documents/GIthub/EEGcontroller/MeasurementSubgroup/Our_measurements/Measurement_prompt/EEGdata-2024-148--14-48-17.csv', delimiter=',')
-data_csv = np.array(data_csv)[:72000,:8] # select the 8 channels and remove 5 samples buffer
+data_csv = data_csv.iloc[:72000,:8] # select the 8 EEG channels and remove 5 samples buffer
 
-#data_csv_car = CAR_filter(data_csv)
-data_csv_detr = detrend(data_csv)
-data_csv_filt = filter(data_csv_detr)
-data_csv_torch = torch.from_numpy(data_csv_filt)
+data_csv_detr = detrend(data_csv) # remove DC component
+data_csv_filt = filter(data_csv_detr) # filter the signal
 
+data_csv = np.array(data_csv)
+data_csv_res = data_csv.reshape(288, 1, 250, 8) # 288 seconds per recording, 250 samples per seconds
 
+data_csv_torch = torch.from_numpy(data_csv_res) # Change to Torch format
+
+logits = data_csv_torch.squeeze(1)
+data_csv_torch = CAR_filter(logits)
+logits = data_csv_torch.unsqueeze(1)
+
+print(logits.shape)
+
+#torch.Size([2880, 1, 256, 16])
+# 2280 seconsd
+# extra dimension
+# 256 samples per sec
+# 16 channels
