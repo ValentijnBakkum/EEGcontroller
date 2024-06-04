4ccd2f4463815ff25561ba62b7a7623ed320f405
jackchen27 <jack.chen2712@gmail.com>
2024-06-04 11:58:28 +0200

    Updated ML integration (filter and shape)

--- MeasurementSubgroup/csv_to_pt.py
+++ MeasurementSubgroup/csv_to_pt.py
@@ -71,26 +71,78 @@ def CAR_filter(logits):
     
     return logits_CAR
 
+def freq_filter(logits):
+
+    # set parameters
+    fs = 250
+    T = 1 / fs
+    L = 256
+
+    # perform FFT
+    fft_logits = torch.fft.fftn(logits, dim=(1,))
+
+    # create frequency mask
+    frequencies = torch.fft.fftfreq(L, d=T)
+    mask = (frequencies >= 0.5) & (frequencies <= 30)
+    full_mask = torch.zeros_like(fft_logits, dtype=torch.bool)
+    full_mask[:, :len(mask), :] = mask[None, :, None]
+    reversed_mask = torch.flip(mask, dims=[0])  
+    full_mask[:, -len(mask):, :] = reversed_mask[None, :, None]
+
+    # apply frequency mask
+    fft_logits_masked = fft_logits * full_mask
+
+    mask = (frequencies >= 52) & (frequencies <= 48)
+    full_mask = torch.zeros_like(fft_logits, dtype=torch.bool)
+    full_mask[:, :len(mask), :] = mask[None, :, None]
+    reversed_mask = torch.flip(mask, dims=[0])  
+    full_mask[:, -len(mask):, :] = reversed_mask[None, :, None]
+
+    # apply frequency mask
+    fft_logits_masked1 = fft_logits_masked * full_mask
+
+    # perform IFFT
+    logits_filtered = torch.fft.ifftn(fft_logits_masked1, dim=(1,)).real
+
+    return logits_filtered
+
 # Read the CSV file into a NumPy array
-data_csv = pd.read_csv('C:/Users/JackC/Documents/GIthub/EEGcontroller/MeasurementSubgroup/Our_measurements/Measurement_prompt/EEGdata-2024-148--14-48-17.csv', delimiter=',')
-data_csv = data_csv.iloc[:72000,:8] # select the 8 EEG channels and remove 5 samples buffer
+data_csv1 = pd.read_csv('C:/Users/JackC/Documents/GIthub/EEGcontroller/MeasurementSubgroup/Our_measurements/Measurement_prompt/EEGdata-2024-149--15-20-21.csv', delimiter=',') 
+data_csv2 = pd.read_csv('C:/Users/JackC/Documents/GIthub/EEGcontroller/MeasurementSubgroup/Our_measurements/Measurement_prompt/EEGdata-2024-149--15-35-40.csv', delimiter=',') 
+data_csv3 = pd.read_csv('C:/Users/JackC/Documents/GIthub/EEGcontroller/MeasurementSubgroup/Our_measurements/Measurement_prompt/EEGdata-2024-149--15-45-38.csv', delimiter=',') 
+data_csv4 = pd.read_csv('C:/Users/JackC/Documents/GIthub/EEGcontroller/MeasurementSubgroup/Our_measurements/Measurement_prompt/EEGdata-2024-149--15-57-42.csv', delimiter=',')
+
+data_csv1 = data_csv1.iloc[:72000,:8] # select the 8 EEG channels and remove 5 samples buffer
+data_csv2 = data_csv2.iloc[:72000,:8] # select the 8 EEG channels and remove 5 samples buffer
+data_csv3 = data_csv3.iloc[:72000,:8] # select the 8 EEG channels and remove 5 samples buffer
+data_csv4 = data_csv4.iloc[:72000,:8] # select the 8 EEG channels and remove 5 samples buffer
+
+data_csv = pd.concat([data_csv1, data_csv2, data_csv3, data_csv4], ignore_index=True)
 
 data_csv_detr = detrend(data_csv) # remove DC component
-data_csv_filt = filter(data_csv_detr) # filter the signal
+#data_csv_filt = filter(data_csv_detr) # filter the signal
+length = 1125
 
 data_csv = np.array(data_csv)
-data_csv_res = data_csv.reshape(288, 1, 250, 8) # 288 seconds per recording, 250 samples per seconds
+data_csv_res = data_csv.reshape(int(length), 1, 256, 8) # 288 seconds per recording, 250 samples per seconds
 
 data_csv_torch = torch.from_numpy(data_csv_res) # Change to Torch format
 
 logits = data_csv_torch.squeeze(1)
-data_csv_torch = CAR_filter(logits)
-logits = data_csv_torch.unsqueeze(1)
+#data_csv_car = CAR_filter(logits)
+data_csv_filt = freq_filter(logits)
+logits = data_csv_filt.unsqueeze(1)
 
 print(logits.shape)
 
 #torch.Size([2880, 1, 256, 16])
-# 2280 seconsd
+# 2880 seconsd could be variable
 # extra dimension
 # 256 samples per sec
 # 16 channels
+
+# 4 recordings from 1 subjects into first dimension
+# with label? and counter, new columns 
+# separate array [1125, 256, 2]
+# Raw recording to send? complete recording
+
