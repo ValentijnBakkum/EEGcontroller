127e0b986d9dbba77d597a89dced710a3906e4c6
C0baltius <martinlittle11@gmail.com>
2024-06-05 09:54:20 +0200

    csv_to_tensor_test is done

--- MeasurementSubgroup/csv_to_tensor_test.py
+++ MeasurementSubgroup/csv_to_tensor_test.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 from scipy import signal
@@ -51,7 +52,7 @@ def filter(df_input):
 def CAR_filter(logits):
     a,b,c = logits.shape
     # initialise zero tensor to hold channel average
-    channel_average = torch.zeros(2880, 256)
+    channel_average = torch.zeros(a, b)
 
     # compute average for each channel
     for i in range(int(logits.size(0))):
@@ -81,7 +82,6 @@ def cursed_reshape(numpy_array):
     ]) * 250
     reshaped_epochs = lists_of_epochs.reshape(24)
     for epoch in reshaped_epochs:
-        print(epoch)
         temp_array = numpy_array[epoch:(epoch + (5 * 256))]
         output_array = np.concatenate((output_array, temp_array.reshape(5, 1, 256, 8)), axis=0)
 
@@ -94,7 +94,8 @@ def cursed_reshape(numpy_array):
 
 
 # Read the CSV file into a NumPy array
-data_csv = pd.read_csv('MeasurementSubgroup/Our_measurements/Measurement_prompt/EEGdata-2024-148--14-48-17.csv', delimiter=',')
+csv_filename = 'EEGdata-2024-148--14-48-17'
+data_csv = pd.read_csv('MeasurementSubgroup/Our_measurements/Measurement_prompt/' + csv_filename + '.csv', delimiter=',')
 data_csv = data_csv.iloc[:72000,:8] # select the 8 EEG channels and remove 5 samples buffer
 
 data_csv_detr = detrend(data_csv) # remove DC component
@@ -103,18 +104,29 @@ data_csv_filt = filter(data_csv_detr) # filter the signal
 data_csv_np = np.array(data_csv_filt)
 
 (data_csv_res, lables) = cursed_reshape(data_csv_np)
-print(data_csv_res.shape)
-print(lables.shape)
+#print(data_csv_res.shape)
+#print(lables.shape)
 
 #data_csv_res = data_csv.reshape(288, 1, 250, 8) # 288 seconds per recording, 250 samples per seconds
 
-data_csv_torch = torch.from_numpy(data_csv_res) # Change to Torch format
+data_torch = torch.from_numpy(data_csv_res) # Change to Torch format
+lables_torch = torch.from_numpy(lables)
 
-logits = data_csv_torch.squeeze(1)
-data_csv_torch = CAR_filter(logits)
-logits = data_csv_torch.unsqueeze(1)
+logits = data_torch.squeeze(1)
+data_torch = CAR_filter(logits)
+logits = data_torch.unsqueeze(1)
 
-print(logits.shape)
+#print(logits.shape)
+
+output_directory = "C:\\Users\\Gebruiker\\Desktop\\Bap\\Code"
+data_filename = csv_filename + '.pt'
+lables_filename = csv_filename + 'labels.pt'
+
+user_data_path = os.path.join(output_directory, data_filename)
+user_labels_path = os.path.join(output_directory, lables_filename)
+
+torch.save(logits, user_data_path)
+torch.save(lables_torch, user_labels_path)
 
 #torch.Size([2880, 1, 256, 16])
 # 2280 seconsd
