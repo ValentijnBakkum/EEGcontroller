4eb56b30d92e928bf8beb5e1aaf12305358f9ceb
jackchen27 <jack.chen2712@gmail.com>
2024-06-24 12:28:00 +0200

    Update escargot3.py

--- PyQT6/escargot3.py
+++ PyQT6/escargot3.py
@@ -118,8 +118,10 @@ class escargot(nn.Module):
         intermediate = self.flatten(intermediate)
         #concatenating in one output layer
         xlstmi = torch.squeeze(x)
+        xlstmi = xlstmi[None,:,:]
         xlstm = self.lstm(xlstmi)
         xlstm = torch.squeeze(xlstm)
+        xlstm = xlstm[None,:]
         intermediate = torch.cat((intermediate,xlstm),dim=1)
         return self.nn(intermediate)
     
@@ -135,14 +137,14 @@ class escargot(nn.Module):
         return nn.Sequential(*list)
     def create_nn(self): #creates the nn used in the model
         return nn.Sequential(nn.Flatten(),
-                                nn.Dropout(p=0.2),
+                                nn.Dropout(p=0.5),
                                 nn.Linear(2506,50,bias=False),
                                 nn.BatchNorm1d(num_features=50),
                                 #nn.ReLU(),
                                 L2NormalizationLayer(),
                                 spline_activation(device = device,grid = 5, input_dim = 50),
-                                nn.Dropout(p=0.2),
-                                nn.Linear(50,2),                         
+                                nn.Dropout(p=0.5),
+                                nn.Linear(50,4),                         
         )
     
 
